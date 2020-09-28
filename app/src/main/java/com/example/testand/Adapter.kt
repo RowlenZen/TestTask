9cd26224183b1f9df6ebf9d21dda22986857287e
Anton <makarovtonyy@gmail.com>
2020-09-28 22:07:58 +0200

    Created  adapter

---
+++ app/src/main/java/com/example/testand/Adapter.kt
@@ -0,0 +1,62 @@
+package com.example.testand
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class Adapter(val clickListener: (Item) -> Unit ) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+
+    var items: List<Item> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_answer, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.combineItems(items[position],clickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun updateList(items : List<Item>){
+        this.items = items
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(element: View) : RecyclerView.ViewHolder(element) {
+
+        val textHead = itemView.findViewById(R.id.text_head) as TextView
+        val date = itemView.findViewById(R.id.text_date) as TextView
+        val totalAnswer = itemView.findViewById(R.id.text_answer) as TextView
+        val image = itemView.findViewById(R.id.image_user) as ImageView
+        val nickname = itemView.findViewById(R.id.text_name) as TextView
+
+        fun combineItems(item : Item, clickListener: (Item) -> Unit) {
+
+            textHead.text = item.title
+            date.text = item.creationDate.toString()
+            totalAnswer.text = item.answerCount.toString()
+            nickname.text = item.owner.displayName
+
+            Glide
+                .with(image)
+                .load(item.owner.profileImage)
+                .into(image)
+
+
+            itemView.setOnClickListener {
+                clickListener(item)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/testand/MainActivity.kt
+++ app/src/main/java/com/example/testand/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val etSearch by lazy { findViewById<EditText>(R.id.et_search) }
     private val bSearch by lazy { findViewById<Button>(R.id.b_search) }
+    private val recycler by lazy { findViewById<RecyclerView>(R.id.rv_element) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val adapter : Adapter = Adapter({element ->
+        //второй экран
+    })
+
 }
