aa096b83139f5d82564ea999f36984f1a6ea1994
Anton <makarovtonyy@gmail.com>
2020-10-02 22:29:58 +0300

    added Toast and edited size of text

--- app/src/main/java/com/example/testand/Adapter.kt
+++ app/src/main/java/com/example/testand/Adapter.kt
@@ -1,10 +1,12 @@
 package com.example.testand
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
